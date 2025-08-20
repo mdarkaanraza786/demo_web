0eac57836e1dc56daeb812501ad5154e7ab70fbe
mdarkaanraza786 <mdarkaanraza786@gmail.com>
2025-08-20 15:29:51 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.title('Welcome to first python project')
+st.title('Welcome to Arkaan,s first website')
+name=st.text_input("Enter your name:")
+fname=st.text_input("Enter your father name:")
+address=st.text_area("Enter the home address")
+classdata=st.selectbox("Select the calss:",(1,2,3,4,5,6,7,8,9,10))
+
+button=st.button("Save")
+
+if button:
+    st.markdown(f"""
+                Name: {name}
+                Father Name: {fname}
+                Address: {address}
+                Class: {classdata}""")
