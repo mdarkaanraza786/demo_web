c2d05a4f508f993a582f209e19342318cec08f9e
mdarkaanraza786 <mdarkaanraza786@gmail.com>
2025-08-21 15:15:44 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas as pd
+from PIL import Image
 
-st.title('Welcome to Arkaan,s first website')
+
+
 name=st.text_input("Enter your name:")
 fname=st.text_input("Enter your father name:")
 address=st.text_area("Enter the home address")
@@ -9,8 +12,20 @@ classdata=st.selectbox("Select the calss:",(1,2,3,4,5,6,7,8,9,10))
 button=st.button("Save")
 
 if button:
-    st.markdown(f"""
-                Name: {name}
-                Father Name: {fname}
-                Address: {address}
-                Class: {classdata}""")
+    data = {
+    "Name": [{name}],
+    "Father Name": [{fname}],
+    "Address": [{address}],
+    "Class": [{classdata}]
+}
+# Convert dictionary to DataFrame
+    df = pd.DataFrame(data)
+    st.write("My details--------------")
+    st.dataframe(df)  # Scrollable & sortable table   
+    # Create some sample data
+    
+# Load the image
+img = Image.open("Arkaan.jpg")  # Replace with your image file path
+
+# Display the image
+st.image(img)    
